98b275a39938a722e9dca2eff315e1c4cc87f73f
Clivern <hello@clivern.com>
2018-05-21 01:02:51 +0200

    add go variables

--- variables.go
+++ variables.go
@@ -4,7 +4,52 @@ import "fmt"
 
 func main() {
 
-    var first_name string = "Joe"
-    last_name := "Doe"
-	fmt.Println(first_name + " " + last_name)
+    /*
+    var (
+        z int
+        k bool
+    )
+    */
+
+    // Constants
+    const (
+        c = 56
+        l = "hko"
+    )
+    //var x, y int
+    //var d int32
+
+    _, b := 34, 35
+
+    fmt.Println(b)
+    fmt.Println(c)
+    fmt.Println(l)
+    fmt.Println(l[0])
+
+
+    var first_name1 string
+    var last_name1 string
+    var age1 int
+    var is_male1 bool
+
+    first_name1 = "Joe"
+    last_name1 = "Doe"
+    age1 = 30
+    is_male1 = true
+
+    first_name2 := "Jessie"
+    last_name2 := "Mole"
+    age2 := 40
+    is_male2 := false
+
+    if is_male1 {
+        fmt.Printf(first_name1 + " " + last_name1 + " is a Male with %d Years old.\n", age1)
+    }else{
+        fmt.Printf(first_name1 + " " + last_name1 + " is a Female with %d Years old.\n", age1)
+    }
+    if is_male2 {
+        fmt.Printf(first_name2 + " " + last_name2 + " is a Male with %d Years old.\n", age2)
+    }else{
+        fmt.Printf(first_name2 + " " + last_name2 + " is a Female with %d Years old.\n", age2)
+    }
 }
